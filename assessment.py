2bdcab46e539cbe00db0fe5fa890666eb9ab5741
Murtaza Godhrawala <murtazagodhrawala94@gmail.com>
2020-07-13 13:53:34 -0400

    initial commit

---
+++ assessment.py
@@ -0,0 +1,13 @@
+import sys
+
+def getFirstSingleLetter(userInput):
+    pass
+
+def rearrangeString(userInput):
+    pass
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        userInput = sys.argv[1]
+        getFirstSingleLetter(userInput)
+        rearrangeString(userInput)
